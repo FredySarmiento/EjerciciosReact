719be02436d2cd8a09a4c5bd48e6adc2f8a351d6
Fredy Sarmiento <fasarmiento93@gmail.com>
2020-09-08 15:51:47 -0500

    Ejercicio 3 completo

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,20 +1,4 @@
-const nombre = 'Fredy';
-const apellido = 'Sarmiento';
+//Arreglos en Javascript
+const arreglo = new Array();
 
-//const nombreCompleto = nombre + ' ' + apellido;
-
-const nombreCompleto = `hola mundo `;
-console.log(`${nombre} ${apellido}`)
-console.log(nombreCompleto)
-
-function getSaludo(){
-    return 'Hola Mundo';
-}
-
-console.log(`Este es el texto que me retorna la funcion ${getSaludo()}`)
-
-function getSaludo( nombre){
-    return 'Hola Mundo';
-}
-
-console.log(`Este es el texto que me retorna la funcion ${getSaludo(nombre)}`)
+console.log(arreglo);
