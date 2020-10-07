a3c494dd1031731c5ca45187e4283f24d0c13106
Fredy Sarmiento <fasarmiento93@gmail.com>
2020-10-07 07:55:01 -0500

    Test parte 1

--- 03-counter-app/src/tests/base/02-template-string.test.js
+++ 03-counter-app/src/tests/base/02-template-string.test.js
@@ -2,11 +2,19 @@ import '@testing-library/jest-dom';
 const { getSaludo } = require('../../base/02-template-string')
 
 describe('Pruebas en 02-template-string.js', () => {
-    test('getSaludo debe eretornar Hola Fredy ', () => {
-        
+    test('getSaludo debe retornar Hola Fredy ', () => {
+        //variable
         const nombre = 'Fredy';
+        //estimulo
         const saludo = getSaludo (nombre);
+        //lo que se espera
         expect (saludo).toBe('Hola '+ nombre);
+    })
+
+    test('getSaludo debe retornar Hola Carlos si no hay argumento ', () => {
+        
+        const saludo = getSaludo();
+        expect (saludo).toBe('Hola Carlos');
     })
     
 })
